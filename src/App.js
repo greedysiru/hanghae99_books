f7223d4a761b5187a7abd7cd78d326d56b5ee901
cje444 <cje444@naver.com>
2021-04-02 03:57:27 -0400

    first-commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         안녕하세요?
         </p>
         <a
           className="App-link"
